d3b21dcea57ede57a0323f1f39c3969a425898cb
DAAC-A00 <wnsdudrkwh@gmail.com>
2023-12-18 16:22:29 +0900

    <feat> ThemeData, Flexible, Expanded, Container-alignment, Container-decoration-BoxDecoration 실습

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
         textTheme: const TextTheme(
           displayLarge: TextStyle(
             color: Color(0xFF232B55),
           ),
         ),
         cardColor: const Color(0xFFF4EDDB),
-        colorSchemeSeed: const Color(0xFFE7626C),
       ),
       home: const HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,26 +11,65 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Column(
         children: [
           Flexible(
             flex: 1,
             child: Container(
-              decoration: const BoxDecoration(color: Colors.red),
-            ),
-          ),
-          Flexible(
-            flex: 2,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.green,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "25:00",
+                style: TextStyle(
+                    color: Theme.of(context).cardColor,
+                    fontSize: 89,
+                    fontWeight: FontWeight.w600),
               ),
             ),
           ),
           Flexible(
-            flex: 1,
+            flex: 3,
             child: Container(
-              decoration: const BoxDecoration(color: Colors.blue),
+              alignment: Alignment.center,
+              child: IconButton(
+                  iconSize: 100,
+                  color: Theme.of(context).cardColor,
+                  onPressed: () {},
+                  icon: const Icon(Icons.play_circle_outline_rounded)),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration:
+                        BoxDecoration(color: Theme.of(context).cardColor),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Poromodo",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color:
+                                Theme.of(context).textTheme.displayLarge?.color,
+                          ),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            fontSize: 50,
+                            color:
+                                Theme.of(context).textTheme.displayLarge?.color,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
